af9a0aa6d5e729b50a45ff3931d0dd8d4d477fa8
wangshen <19900222@163.com>
2020-05-24 10:47:53 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET,produces="text/plain;charset=UTF-8")
+    public String hello(@RequestParam(value = "userName") String userName){
+
+        return "我 的第一个Java后台"+userName;
+    }
+    @RequestMapping(value = "/getList",method = RequestMethod.GET)
+    public String getList(@RequestParam(value = "userId") String data){
+
+        return "我 的第一个Java后台"+data;
+    }
+}
